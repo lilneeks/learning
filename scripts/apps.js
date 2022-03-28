d79952df65fe0a74ebc941a40378651d2a549413
lilneeks <nickodc96@gmail.com>
2022-03-28 03:47:05 -0700

    commit

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,14 +1,42 @@
-var list = [];
-document.getElementById("output").innerHTML = list;
+
+
+function outPut(){
+    document.getElementById("output").innerHTML = list;
+}
 
 function yesFunction() {
     list.push('YES');
-    document.getElementById("output").innerHTML = list;
+    outPut();
 }
 
 function noFunction() {
-    list.push('NO');
-    document.getElementById("output").innerHTML = list;
+    list.push("NO");
+    outPut();
 }
 
-console.log(list)
+function clearList(){
+    list.length =0;
+    document.getElementById("output").innerHTML = "";
+
+}
+
+
+function consoleFunction(){
+console.log(list);
+}
+
+
+let list = [];
+
+function getVote() {
+    let map = {};
+    // console.log(list);
+  for (let i = 0; i < list.length; i++) {
+      let item = list[i];
+      map[item] = (map[item] + 1) || 1;
+    }
+    document.getElementById("output").textContent = JSON.stringify(map);
+    return map;
+  }
+
+//   getVote(list);
